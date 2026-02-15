a47c4df2f3177acdb8888ec23fee151c0b7dccd4
Alex Vaystikh <alex.vaystikh@gmail.com>
2026-02-15 13:17:35 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/fuse-bead-studio/',
   server: {
     port: 3000,
     host: '0.0.0.0',
